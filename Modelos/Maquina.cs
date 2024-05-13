ee69228fd1d8f75da292dc985841ee9ce381e82a
Sofia Laino <sofialaino@outlook.com>
2024-05-13 19:45:21 -0300

    Agregar archivos de proyecto.

---
+++ Modelos/Maquina.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObligatorioAgusFioSofi
+{
+    internal class Maquina
+    {
+        public int IdMaquina { get; set; }
+        public Local LocalAsociado { get; set; }
+        public DateTime FechaCompra { get; set; }
+        public int PrecioCompra { get; set; }
+        public int VidaUtil { get; set; }
+        public TipoMaquina TipoMaquina { get; set; }
+        public string Disponible { get; set; }
+
+    }
+}
---
+++ Modelos/Socio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObligatorioAgusFioSofi
+{
+    internal class Socio : Persona
+    {
+        public int IdSocio { get; set; }
+        public string Tipo { get; set; }
+        public string Email { get; set; }
+        public Local LocalAsociado { get; set; }
+    }
+}
